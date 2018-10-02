f6a92571bd78d65cc52abe54d5a9a229e1343408
bguids91 <bguids91@gmail.com>
2018-10-02 02:33:35 +0000

    Adds html template with styling

---
+++ src/Message.jsx
@@ -0,0 +1,4 @@
+<div class="message">
+  <span class="message-username">Anonymous1</span>
+  <span class="message-content">I won't be impressed with technology until I can download food.</span>
+</div>
