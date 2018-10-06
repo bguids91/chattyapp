ab00b780c1beff5ebc4724a2ab7f4dba48601c69
bguids91 <bguids91@gmail.com>
2018-10-06 21:15:20 +0000

    Adds new gif

--- src/Message.jsx
+++ src/Message.jsx
@@ -8,7 +8,7 @@ class Message extends Component {
     if (this.props.content === "/hungry") {
     return (<div className="message">
       <span className="message-username">{this.props.username}</span>
-      <span className="message-content"><img src="https://media3.giphy.com/media/3BaaBb6y8J9Fm/giphy.gif?cid=3640f6095bb923036c3365736fc380bb" /></span>
+      <span className="message-content"><img src="https://media0.giphy.com/media/78BrUoRs5II2A/giphy.gif?cid=3640f6095bb924be653839576b0346a2" /></span>
     </div>)
     } else {
       return (<div className="message">
